0669348814cebb0a52a85dafef79ea87df7c7b63
Sh-Revo <shandov@i.ua>
2020-10-05 20:32:05 +0300

    Some fix

--- src/main/java/com/shandov/jdbcHomework/Main.java
+++ src/main/java/com/shandov/jdbcHomework/Main.java
@@ -21,6 +21,7 @@ public class Main {
         //System.out.println("\nСпикок Middle разработчиков " + developersService.getAllMiddleDevelopers("Middle"));
 
 
+
         //-----Projects
 
        // projectsService.insertIntoProjects();
